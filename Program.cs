cdae495a295ef08f5848a3632bcd27354d8d3d84
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 20:02:20 -0300

    Adiciona atividade Sinal

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            Single.TryParse(Console.
+            ReadLine()
+            .ToString(), out Single _numeroDigitado);
+
+            if(_numeroDigitado > 0){
+                Console.WriteLine("Positivo");
+            }
+            else if(_numeroDigitado < 0){
+                Console.WriteLine("Negativo");
+            }
+            else{
+                Console.WriteLine("Zero");
+            }   
+        }
+    }
+}
